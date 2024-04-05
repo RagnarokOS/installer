e36dc95ed990234eb5eb89e7390a0749660c2778
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-04-05 12:30:40 -0400

    add setup hook

---
+++ hooks/setup01.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Customize before the setup phase
+# $Ragnarok: setup01.sh,v 1.1 2024/04/05 16:30:28 lecorbeau Exp $
+
+set -e
+
+# Copy apt sources and keys to the chroot
+mkdir -p "$1"/etc/apt
+cp -r ../src/etc/apt/sources.list.d "$1"/etc/apt/
+cp -r ../src/etc/apt/trusted.gpg.d/ "$1"/etc/apt/
+
+# Creating /etc/mailname. bsd-mailx and dma are installed non-interactively and we need
+# this file to prevent dpkg from setting mailname to 'root' when dma is installed.
+touch "$1"/etc/mailname
