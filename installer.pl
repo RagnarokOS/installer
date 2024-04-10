ee0eb50bf7d51cc6bf02714cfad6a94d7281f744
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-04-10 12:12:30 -0400

    install.pl: add mmdebstrap cmd

--- installer.pl
+++ installer.pl
@@ -16,6 +16,7 @@ my $conf = Config::General->new(
 # Get all config values and declare them. This will make the rest of the
 # code easier to read.
 my %config	= $conf->getall;
+my $codename	= $config{'codename'};
 my $flavour	= $config{'flavour'};
 my $variant	= $config{'variant'};
 my $components	= $config{'components'};
@@ -46,6 +47,19 @@ sub ask {
 	chomp(my $answer = <STDIN>);
 
 	return $answer;
+}
+
+# call mmdebstrap
+sub debstrap {
+	system("/usr/bin/mmdebstrap", "--variant=$variant",
+		"--components=$components", "--hook-directory=hooks/",
+		"$flavour", "/mnt/",
+		"deb https://ragnarokos.github.io/base/deb/ ${codename} main", 
+		"deb https://ragnarokos.github.io/xserv/deb/ ${codename} main",
+		"deb http://deb.debian.org/debian/ ${flavour} main non-free-firmware",
+		"deb http://security.debian.org/ ${flavour}-security main non-free-firmware",
+		"deb http://deb.debian.org/debian/ ${flavour}-updates main non-free-firmware") == 0
+			or error("mmdebstrap failed, $!");
 }
 
 ## Actual installer
