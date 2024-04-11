3c14aa99749249463a22743993befc826e8d6db6
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-04-11 10:34:25 -0400

    setup01.sh: set debconf frontend to Readline

--- hooks/setup01.sh
+++ hooks/setup01.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Customize before the setup phase
-# $Ragnarok: setup01.sh,v 1.1 2024/04/05 16:30:28 lecorbeau Exp $
+# $Ragnarok: setup01.sh,v 1.2 2024/04/11 14:34:21 lecorbeau Exp $
 
 set -e
 
@@ -9,6 +9,9 @@ set -e
 mkdir -p "$1"/etc/apt
 cp -r ../src/etc/apt/sources.list.d "$1"/etc/apt/
 cp -r ../src/etc/apt/trusted.gpg.d/ "$1"/etc/apt/
+
+# Set the default debconf frontend to Readline
+chroot "$1" echo 'debconf debconf/frontend select Readline' | debconf-set-selections
 
 # Creating /etc/mailname. bsd-mailx and dma are installed non-interactively and we need
 # this file to prevent dpkg from setting mailname to 'root' when dma is installed.
