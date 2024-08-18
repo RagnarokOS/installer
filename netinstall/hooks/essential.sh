50f32ce5b08c6b606b21bddfd7b599b19f4bccb0
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-08-18 13:42:15 -0400

    add essential hook
    run the commands that need to be executed after the 'essential' packages
    were installed, but before doing anything else.

---
+++ netinstall/hooks/essential.sh
@@ -0,0 +1,47 @@
+#!/bin/ksh
+
+# $Ragnarok
+
+. /lib/ragnarok-installer/funcs
+
+CONF=${CONF:-install.conf}
+
+# Set locales
+set_locales() {
+	local _locale _charset
+
+	_locale=$(get_val Locale "$CONF")
+	_charset=$(awk '/Locale/ { print $4 }' "$CONF")
+
+	msg "Setting up locales..."
+	echo "locales locales/default_environment_locale select $_locale" \
+		| chroot "$1" debconf-set-selection
+	echo "locales locales_to_be_generated multiselect $_locale $_charset" \
+		| chroot "$1" debconf-set-selections
+	chroot "$1" apt-get install locales -y
+	chroot "$1" dpkg-reconfigure -f noninteractive locales
+}
+
+# Setting the timezone
+set_tz() {
+	local _tz _area _zone
+
+	_tz=$(get_val Timezone "$CONF")
+	_area=${_tz%%/*}
+	_zone=${_tz##/*}
+
+	msg "Setting the timezone..."
+	echo "tzdata tzdata/Areas select $_area" | chroot "$1" debconf-set-selections
+	echo "tzdata tzdata/Zones/$_area select $_zone" | chroot "$1" debconf-set-selections
+	echo 'tzdata tzdata/Zones/Etc select UTC' | chroot "$1" debconf-set-selections
+
+	# This has to be done or else dpkg-reconfigure insists on using Etc
+	# as the default timezone for whatever stupid reason.
+	echo "${_area}/${_zone}" > "$1"/etc/timezone
+	chroot "$1" ln -sf /usr/share/zoneinfo/"${_area}"/"${_zone}" /etc/localtime
+
+	chroot "$1" dpkg-reconfigure -f noninteractive tzdata
+}
+
+set_locales
+set_tz
