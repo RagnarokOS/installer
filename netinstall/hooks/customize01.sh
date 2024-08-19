7a9dba4fd927bbc588efbe463cf868625d31150e
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-08-19 11:20:50 -0400

    add customize01.sh
    Run the basic system customization commands.

---
+++ netinstall/hooks/customize01.sh
@@ -0,0 +1,28 @@
+#!/bin/ksh
+
+# $Ragnarok: customize01.sh,v 1.1 2024/08/19 15:20:45 lecorbeau Exp $
+
+set -e
+
+# Set the default debconf frontend to Readline
+echo 'debconf debconf/frontend select Readline' | chroot "$1" debconf-set-selections
+
+# Enable the wheel group.
+sed -i '15 s/^# //' "$1"/etc/pam.d/su
+chroot "$1" addgroup --system wheel
+
+# Make sure ksh is the default shell for root.
+sed -i 's/bash/ksh/g' "$1"/etc/passwd
+
+# Install ragnarok-base. 
+chroot "$1" apt-get -o Apt::Install-Recommends="true" \
+	-o Dpkg::Options::="--force-confdef" install ragnarok-base -y
+
+# Create signify symlink
+chroot "$1" ln -sf /usr/bin/signify-openbsd /usr/bin/signify
+
+# Install man-db dummy
+_mdb_dummy="man-db_999+ragnarok01_amd64.deb"
+cp /var/db/dummies/"$_mdb_dummy" "$1"/
+chroot "$1" apt-get install ./"$_mdb_dummy" -y
+chroot "$1" rm "$_mdb_dummy"
