2ba64e05d6d3d8fa5a62daa62940b41e2718a546
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2024-08-21 14:04:42 -0400

    move locales/tz setup to customize02 hook

--- netinstall/hooks/customize02.sh
+++ netinstall/hooks/customize02.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh
 
-# $Ragnarok: customize02.sh,v 1.7 2024/08/20 23:09:19 lecorbeau Exp $
+# $Ragnarok: customize02.sh,v 1.8 2024/08/21 18:04:35 lecorbeau Exp $
 
 . /lib/ragnarok-installer/funcs
 
@@ -8,6 +8,9 @@
 CONF=${CONF:-/install.conf}
 _locale=$(get_val Locale "$CONF")
 _charset=$(awk '/Locale/ { print $4 }' "$CONF")
+_tz=$(get_val Timezone "$CONF")
+_area=${_tz%%/*}
+_zone=${_tz##/*}
 _keymap=$(get_val KB_Layout "$CONF")
 _variant=$(get_val KB_Variant "$CONF")
 _hostname=$(get_val Hostname "$CONF")
@@ -77,11 +80,24 @@ fi
 
 # Set locales
 msg "Setting up locales..."
+echo "locales locales/default_environment_locale select $_locale" \
+	| chroot "$1" debconf-set-selections
+echo "locales locales/locales_to_be_generated multiselect $_locale $_charset" \
+	| chroot "$1" debconf-set-selections
 chroot "$1" apt-get install locales -y
+# Remove locale.gen. It'll be recreated after dpkg-reconfigure
+rm "$1"/etc/locale.gen
 chroot "$1" dpkg-reconfigure -f noninteractive locales
 
 # Setting the timezone
 msg "Setting the timezone..."
+echo "tzdata tzdata/Areas select $_area" | chroot "$1" debconf-set-selections
+echo "tzdata tzdata/Zones/$_area select $_zone" | chroot "$1" debconf-set-selections
+echo 'tzdata tzdata/Zones/Etc select UTC' | chroot "$1" debconf-set-selections
+# This has to be done or else dpkg-reconfigure insists on using Etc
+# as the default timezone for whatever stupid reason.
+echo "${_area}/${_zone}" > "$1"/etc/timezone
+chroot "$1" ln -sf /usr/share/zoneinfo/"${_area}"/"${_zone}" /etc/localtime
 chroot "$1" dpkg-reconfigure -f noninteractive tzdata
 
 # Setup the console
